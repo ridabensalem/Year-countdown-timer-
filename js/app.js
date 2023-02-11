9aead1e35f162a5943c0f76dc52985c145db1536
ridabensalem <ridasbnesalmas@gmail.com>
2023-02-11 19:45:22 +0100

    Set up linters

--- js/app.js
+++ js/app.js
@@ -1,22 +1,20 @@
-// Get the new year 
+// Get the new year
 const getNewYear = () => {
-    const currentYear = new Date().getFullYear();
-    return new Date(`January 01 ${currentYear + 1} 00:00:00`);
+  const currentYear = new Date().getFullYear();
+  return new Date(`January 01 ${currentYear + 1} 00:00:00`);
 };
 
 // update the year element
-const year = document.querySelector('.year');
+const year = document.querySelector(".year");
 year.innerHTML = getNewYear().getFullYear();
 
 // select elements
-const app = document.querySelector('.countdown-timer');
-const message = document.querySelector('.message');
-const heading = document.querySelector('h1');
-
-
+const app = document.querySelector(".countdown-timer");
+const message = document.querySelector(".message");
+const heading = document.querySelector("h1");
 
 const render = (time) => {
-    app.innerHTML = `
+  app.innerHTML = `
         <div class="count-down">
             <div class="timer">
                 <h2 class="days">${time.days}</h2>
@@ -38,31 +36,30 @@ const render = (time) => {
         `;
 };
 
-
 const showMessage = () => {
-    message.innerHTML = `Happy New Year ${newYear}!`;
-    app.innerHTML = '';
-    heading.style.display = 'none';
+  message.innerHTML = `Happy New Year ${newYear}!`;
+  app.innerHTML = "";
+  heading.style.display = "none";
 };
 
 const hideMessage = () => {
-    message.innerHTML = '';
-    heading.style.display = 'block';
+  message.innerHTML = "";
+  heading.style.display = "block";
 };
 
 const complete = () => {
-    showMessage();
+  showMessage();
 
-    // restart the countdown after showing the 
-    // greeting message for a day ()
-    setTimeout(() => {
-        hideMessage();
-        countdownTimer.setExpiredDate(getNewYear());
-    }, 1000 * 60 * 60 * 24);
+  // restart the countdown after showing the
+  // greeting message for a day ()
+  setTimeout(() => {
+    hideMessage();
+    countdownTimer.setExpiredDate(getNewYear());
+  }, 1000 * 60 * 60 * 24);
 };
 
 const countdownTimer = new CountDown(
-    getNewYear(),
-    render,
-    complete
+  getNewYear(),
+  render,
+  complete,
 );
--- js/countdown.js
+++ js/countdown.js
@@ -1,63 +1,62 @@
 class CountDown {
-    constructor(expiredDate, onRender, onComplete) {
-        this.setExpiredDate(expiredDate);
+  constructor(expiredDate, onRender, onComplete) {
+    this.setExpiredDate(expiredDate);
 
-        this.onRender = onRender;
-        this.onComplete = onComplete;
+    this.onRender = onRender;
+    this.onComplete = onComplete;
+  }
+
+  setExpiredDate(expiredDate) {
+    // get the current time
+    const currentTime = new Date().getTime();
+
+    // calculate the remaining time
+    this.timeRemaining = expiredDate.getTime() - currentTime;
+
+    this.timeRemaining > 0
+      ? this.start()
+      : this.complete();
+  }
+
+  complete() {
+    if (typeof this.onComplete === "function") {
+      onComplete();
     }
+  }
 
-    setExpiredDate(expiredDate) {
-        // get the current time
-        const currentTime = new Date().getTime();
+  getTime() {
+    return {
+      days: Math.floor(this.timeRemaining / 1000 / 60 / 60 / 24),
+      hours: Math.floor(this.timeRemaining / 1000 / 60 / 60) % 24,
+      minutes: Math.floor(this.timeRemaining / 1000 / 60) % 60,
+      seconds: Math.floor(this.timeRemaining / 1000) % 60,
+    };
+  }
 
-        // calculate the remaining time 
-        this.timeRemaining = expiredDate.getTime() - currentTime;
-
-        this.timeRemaining > 0 ?
-            this.start() :
-            this.complete();
+  update() {
+    if (typeof this.onRender === "function") {
+      this.onRender(this.getTime());
     }
+  }
 
+  start() {
+    // update the countdown
+    this.update();
 
-    complete() {
-        if (typeof this.onComplete === 'function') {
-            onComplete();
-        }
-    }
-    getTime() {
-        return {
-            days: Math.floor(this.timeRemaining / 1000 / 60 / 60 / 24),
-            hours: Math.floor(this.timeRemaining / 1000 / 60 / 60) % 24,
-            minutes: Math.floor(this.timeRemaining / 1000 / 60) % 60,
-            seconds: Math.floor(this.timeRemaining / 1000) % 60
-        };
-    }
+    //  setup a timer
+    const intervalId = setInterval(() => {
+      // update the timer
+      this.timeRemaining -= 1000;
 
-    update() {
-        if (typeof this.onRender === 'function') {
-            this.onRender(this.getTime());
-        }
-    }
+      if (this.timeRemaining < 0) {
+        // call the callback
+        complete();
 
-    start() {
-        // update the countdown
+        // clear the interval if expired
+        clearInterval(intervalId);
+      } else {
         this.update();
-
-        //  setup a timer
-        const intervalId = setInterval(() => {
-            // update the timer  
-            this.timeRemaining -= 1000;
-
-            if (this.timeRemaining < 0) {
-                // call the callback
-                complete();
-
-                // clear the interval if expired
-                clearInterval(intervalId);
-            } else {
-                this.update();
-            }
-
-        }, 1000);
-    }
+      }
+    }, 1000);
+  }
 }
